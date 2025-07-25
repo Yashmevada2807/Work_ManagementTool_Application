7dad8f773dabcd3c50f0f818e89063c059241692
Yashmevada2807 <yashmevada07@gmail.com>
2025-07-25 19:05:33 +0530

    sm changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Inbox from './components/Inbox'
 import WorkFlowContainer from './components/WorkFlowContainer'
+import { DndContext } from '@dnd-kit/core'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <>
-    <div className='w-full min-h-screen bg-gray-950 px-10 py-15 flex justify-between'>
-      <div className='w-[1/2] border border-white'>
-        <Inbox/>
+      <div className='w-full min-h-screen bg-center bg-cover bg-zinc-900  px-10 py-1 md:flex  gap-4 justify-between'
+      // style={{backgroundImage : `url('public/img/hex-background-networking (1).jpg')`}}
+      >
+          <div className=' w-full mb-4 md:w-[25%] '>
+            <Inbox />
+          </div>
+          <div className='w-full md:w-[75%] '>
+            <WorkFlowContainer />
+          </div>
       </div>
-      <div className='w-[1/2] border border-white'>
-      <WorkFlowContainer/>
-      </div>
-    </div>
     </>
   )
 }
