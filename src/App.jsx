6a3b05594a14ea2c504386a947d94a62e4cd7896
Yashmevada2807 <yashmevada07@gmail.com>
2025-07-29 16:38:17 +0530

    am changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Inbox from './components/Inbox'
 import WorkFlowContainer from './components/WorkFlowContainer'
 import { DndContext } from '@dnd-kit/core'
+import { ToastContainer, Bounce } from 'react-toastify'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,15 +13,28 @@ function App() {
 
   return (
     <>
-      <div className='w-full min-h-screen bg-center bg-cover bg-zinc-900  px-10 py-1 md:flex  gap-4 justify-between'
+      <div className='w-full min-h-screen bg-center bg-cover bg-gray-950 md:flex  gap-1 justify-between'
       // style={{backgroundImage : `url('public/img/hex-background-networking (1).jpg')`}}
       >
-          <div className=' w-full mb-4 md:w-[25%] '>
-            <Inbox />
-          </div>
-          <div className='w-full md:w-[75%] '>
-            <WorkFlowContainer />
-          </div>
+        <div className=' w-full  md:w-[25%] '>
+          <Inbox />
+        </div>
+        <div className='w-full md:w-[75%] '>
+          <WorkFlowContainer />
+        </div>
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={true}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+          transition={Bounce}
+        />
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
+import { persistor } from './app/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
-  
-    <Provider store={store}>
+
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
       <App />
-    </Provider>
-  
+    </PersistGate>
+  </Provider>
+
 )
