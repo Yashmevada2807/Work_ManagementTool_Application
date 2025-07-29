75690c49c2571fda5838e71fa045d11edd9ddc13
Yashmevada2807 <yashmevada07@gmail.com>
2025-07-29 16:39:37 +0530

    added redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { todoReducer } from "../features/todos/todoSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 
-export const store  = configureStore({
-    reducer: {
-        todos: todoReducer
-    },
+const todoPersistConfig = {
+    key: 'Todos',
+    storage
+}
+
+const rootReducer = combineReducers({
+    todos: persistReducer(todoPersistConfig, todoReducer)
 })
+
+
+export const store = configureStore({
+    reducer: rootReducer
+})
+
+export const persistor = persistStore(store)
